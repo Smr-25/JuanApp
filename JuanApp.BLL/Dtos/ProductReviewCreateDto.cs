8e0684e7220150a6455d7941ebde272cb02f3352
Smr-25 <samiraa@code.edu.az>
2026-05-05 09:12:44 +0400

    05.05.26 commit

--- JuanApp.BLL/Dtos/ProductReviewCreateDto.cs
+++ JuanApp.BLL/Dtos/ProductReviewCreateDto.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace JuanApp.BLL.Dtos;
 
 public class ProductReviewCreateDto
 {
+    [Required]
+    [Range(1, int.MaxValue, ErrorMessage = "Product ID must be valid")]
     public int ProductId { get; set; }
+
+    [Required]
+    [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5 stars")]
     public int Rating { get; set; }
-    public string? Comment { get; set; } = string.Empty;
+
+    [StringLength(1000, MinimumLength = 5, ErrorMessage = "Comment must be between 5 and 1000 characters")]
+    public string? Comment { get; set; }
 }
 
